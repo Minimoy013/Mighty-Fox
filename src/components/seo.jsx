b74cb0baabb4e147c031046e5a5901e31de37038
Minimoy013 <32453072+Minimoy013@users.noreply.github.com>
2023-04-18 21:26:46 +0200

    Update seo.jsx
    suppression TAG

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -14,17 +14,6 @@ const SEO = ({ pageTitle, font }) => (
         />
         {font && <link href={font} rel="stylesheet" />}
         <link rel="icon" href="/favicon.ico" />
-
-        {/* <!-- Google tag (gtag.js) --> */}
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-141195271-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments)}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-141195271-1');
-</script>
- 
     </Head>
 )
 
