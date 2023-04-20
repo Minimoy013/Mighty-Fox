a97bae3ecd5ad8fbb0eee4dccf8734902cf4cea4
Minimoy013 <32453072+Minimoy013@users.noreply.github.com>
2023-04-20 12:18:51 +0200

    correction fautes blog
    correction

--- src/data/blog-data.js
+++ src/data/blog-data.js
@@ -1,6 +1,4 @@
 const blog_data = [
- 
-  // home
   {
     id: 1,
     img: "/assets/images/blog/01_comment_ameliorer_son_SEO.webp",
@@ -10,14 +8,14 @@ const blog_data = [
     title: "Comment améliorer le référencement de son site internet ?",
     date: "30 Mars 2023",
     sm_desc: "Le référencement kézako ? SEO... Vous ne le voyez toujours pas ? Plus simplement, il s'agit de référencement nature (gratuit), ou de ce qu'on appelle...",
-    intro: "Le référencement, kézako ? SEO... Vous ne le voyez toujours pas ? Plus simplement, il s'agit de référencement naturel (gratuit), ou de ce qu'on appelle les recherches orgraniques. Ces techniques sont utilisées pour améliorer le positionnement d'un site Web sur les moteurs de recherche tels que Google. ",
+    intro: "Le référencement, kézako ? SEO... Vous ne le voyez toujours pas ? Plus simplement, il s'agit de référencement naturel (gratuit), ou de ce qu'on appelle les recherches organiques. Ces techniques sont utilisées pour améliorer le positionnement d'un site Web sur les moteurs de recherche tels que Google. ",
     tipsTitle: " 3 astuces pour améliorer votre référencement et vous propulser vers le haut !",
-    tips1: "Travaillez vos liens : Les liens externes ou 'Backlinks' sont un plus pour les moteurs de recherche. Grâce à eux vous ferez figure de référence, mais assurez vous d'être linké depuis des site provenant de domaines à forte notoriété. Pensez à améliorer le maillage internet des pages au sein de votre propre site Web, ils servent à connecter différentes pages au sein de votre domaine.",
+    tips1: "Travaillez vos liens : Les liens externes ou 'Backlinks' sont un plus pour les moteurs de recherche. Grâce à eux vous ferez figure de référence, mais assurez vous d'être linké depuis des sites provenant de domaines à forte notoriété. Pensez à améliorer le maillage internet des pages au sein de votre propre site Web, ils servent à connecter différentes pages au sein de votre domaine.",
     tips2:"Optimisez la qualité de vos contenus : Recherchez les meilleurs mots-clés de longue traîne en lien avec l’intention de recherche de votre cible. Le contenu doit être créatif et pertinant, évitez de dupliquer sur d'autre site internet ou bien d'utiliser des outils d'éditions automatisés comme chatGPT.",
-    tips3: "Optimisez vos pages au niveau technique : Le SEO implique souvent l'optimisation de la structure, de la navigation et du contenu de votre site web. Verifier la rapidité de votre site internet sur Google Page Speed Insight. Alléger le poids des images, optez pour une résolution de 72px et privilégiez le format Webp (n'oubliez pas de mettre des titres et des alt cohérents à vos images). Optimisez le code / supprimez les plugins inutilisés. Utilisez des plugin de mise en cache et optez pour du chargement asynchrone. Structurez vos pages Web et vos balises H1, complétez vos méta déscriptions et créez un sitemap XML ",
+    tips3: "Optimisez vos pages au niveau technique : Le SEO implique souvent l'optimisation de la structure, de la navigation et du contenu de votre site web. Verifiez la rapidité de votre site internet sur Google Page Speed Insight. Alléger le poids des images, optez pour une résolution de 72px et privilégiez le format Webp (n'oubliez pas de mettre des titres et des alt cohérents à vos images). Optimisez le code / supprimez les plugins inutilisés. Utilisez des plugins de mise en cache et optez pour du chargement asynchrone. Structurez vos pages Web et vos balises H1, complétez vos métas descriptions et créez un sitemap XML ",
     quote: "Le SEO n'est pas une dépense, c'est un investissement. - Chris J. EVERETT, Co-fondateur de Captivate SEO.",
-    conclusionTitle: "La qualité plutot que la quantité",
-    conclusion: " La tendance n'est plus à l'inondation sur le net, mais plutôt à la qualité. Votre site internet est le prolongement des valeurs de votre entreprise, et votre but est d’offrir une expérience enrichissante et une navigation fluide à vos utilisateurs. Ayez en tête que ce travail est titanesque et de longue haleine. Vous ne verrez pas les résultats rapidement, alors ne vous décourragez pas, soyez constant et patient, et vous serrez récompensé par une communauté grandissante et fidéle et engagée.",
+    conclusionTitle: "La qualité plutôt que la quantité",
+    conclusion: " La tendance n'est plus à l'inondation sur le net, mais plutôt à la qualité. Votre site internet est le prolongement des valeurs de votre entreprise, et votre but est d’offrir une expérience enrichissante et une navigation fluide à vos utilisateurs. Ayez en tête que ce travail est titanesque et de longue haleine. Vous ne verrez pas les résultats rapidement, alors ne vous découragez pas, soyez constant et patient, et vous serrez récompensé par une communauté grandissante, fidèle et engagée.",
     feature: "/assets/images/blog/feature-1.webp",
     shareFacebook: "https://www.facebook.com/share.php?u=https://www.mighty-fox.com/blog-details/1",
     shareTwitter: "https://twitter.com/intent/tweet?text=Comment-améliorer-le-référencement-de-son-site-internet-?&amp;url=http://www.mighty-fox.com/blog-details/1",
@@ -27,7 +25,6 @@ const blog_data = [
     home_4:true,
     large:false,
   },
-
   {
     id: 2,
     img: "/assets/images/blog/03_peut_on_utiliser_chat_gpt_dans_une_strategie_seo.webp",
@@ -40,11 +37,11 @@ const blog_data = [
     intro: "ChatGPT est un générateur de texte d'intelligence artificielle développé par OpenAI et a été lancé le 30 novembre 2022. ChatGPT peut générer du contenu à volonté et sur n'importe quel thème. Avec cette nouvelle technologie, il est légitime de se poser la question si oui ou non il faut l'utiliser pour générer du contenu sans pénaliser son référencement naturel.",
     quote: "La création d'une intelligence artificielle serait le plus grand événement de l'histoire de l'humanité. Mais il pourrait aussi être l'ultime. - Stephan HAWKING.",
     tipsTitle: "Trois astuces pour utiliser au mieux ChatGPT",
-    tips1: "Ne faites pas de copier/coller : N'allée pas croire que ChatGPT peut remplacer un rédacteur humain. Certes vous allée générer du texte, mais pensez que si vous l'avez fait, des miliers de personnes peuvent faire pareil. Vous prendrez le risque de poster du contenu impersonnel et uniformisé et nous savons que seules des publications à forte valeur ajoutée vous permettront de vous démarquer de la concurrence. Utilisez plutôt Chat GPT comme un outil pour vous aider à trouver des idées, à les structurer, les optimisez... et non comme un outil de remplacement sur la partie rédactionnelle.",
+    tips1: "Ne faites pas de copier/coller : N'allée pas croire que ChatGPT peut remplacer un rédacteur humain. Certes vous allée générer du texte, mais pensez que si vous l'avez fait, des milliers de personnes peuvent faire pareil. Vous prendrez le risque de poster du contenu impersonnel et uniformisé et nous savons que seules des publications à forte valeur ajoutée vous permettront de vous démarquer de la concurrence. Utilisez plutôt Chat GPT comme un outil pour vous aider à trouver des idées, à les structurer, les optimisez... et non comme un outil de remplacement sur la partie rédactionnelle.",
     tips2:"Vérifiez les sources : Si vous n'avez pas les moyens d'avoir un rédacteur professionnel et que vous souhaitez utiliser chatGPT pour le rédactionnel, pensez que ChatGPT fonctionne sur le deeplearning et que certaines de ses sources d'apprentissages peuvent être obsolètes. Faites des recherches pour confirmer ou infirmer les informations collectées et enrichissez au passage votre contenu avec diverses sources fiables. Il est important de s’assurer que le contenu généré par IA soit de qualité, unique et utile pour les utilisateurs pour éviter d'être sanctionné par Google.",
     tips3: "Soyez le plus clair et précis possible : Utiliser des verbes forts et exprimez vous clairement ce que vous souhaitez obtenir. Si la réponse n'est pas satisfaisante, indiquer à l'IA ce qui a causé l'erreur d'interprétation. Il est également conseillé de limiter la taille des réponses pour éviter les erreurs. En cas d'échec répétés, n'hésitez pas à ouvrir un nouveau chat et repartir à zéro afin de reformuler ou de reposer les questions pour obtenir des réponses plus précises.",
     conclusionTitle: "Google peut-il détecter le contenu écrit par l’IA ?",
-    conclusion: "Oui Google peut détecter le contenu généré par une IA, mais le sanctionne-t-il ? Oui et Non, encore une fois Google cherche à proposer à ses utilisateurs du contenu de qualité. Si Google juge que le contenu est sans valeur ajouté, alors oui il vous pénalisera. Donc, en définitive, si vous utilisez l’IA pour produire vos textes, demandez-vous si votre cotenu est original.",
+    conclusion: "Oui Google peut détecter le contenu généré par une IA, mais le sanctionne-t-il ? Oui et Non, encore une fois Google cherche à proposer à ses utilisateurs du contenu de qualité. Si Google juge que le contenu est sans valeur ajoutée, alors oui il vous pénalisera. Donc, en définitive, si vous utilisez l’IA pour produire vos textes, demandez-vous si votre contenu est original.",
     feature: "/assets/images/blog/feature-2.webp",
     shareFacebook: "https://www.facebook.com/share.php?u=https://www.mighty-fox.com/blog-details/2",
     shareTwitter: "https://twitter.com/intent/tweet?text=Comment-booter-ses-posts-twitter-?&amp;url=http://www.mighty-fox.com/blog-details/2",
@@ -54,7 +51,6 @@ const blog_data = [
     home_4:true,
     large:false,
   },
-
   {
     id: 3,
     img: "/assets/images/blog/02_comment_booster_son_compte_twitter.webp",
@@ -63,11 +59,11 @@ const blog_data = [
     category: "Réseaux Sociaux",
     title: "Comment Booster ses posts Twitter ?",
     date: "13 avril 2023",
-    sm_desc: "500 millions de tweets sont postés chaque jours sur Twitter. Mais alors comment faire en sorte que son post soit visible dans ce flot d'information ?",
-    intro: "500 millions de tweets sont postés chaque jours sur Twitter. Mais alors comment faire en sorte que son post soit visible dans ce flot d'information ? Il faut savoir que l'algorithme de Twitter note chaque post selon un barème. Selon les interactions qu'il reçoit celui-ci obtient un score, et plus le score est grand plus il a de chance d'être mise en avant. Un like = 0.5 point, un retweet = 1 point, 1 clic + 1 like + 1 commentaire = 11 points si vous répondez à un commentaire laissé sur votre post = 75 points. Attention, si vous pouvez gagnez des points, vous pouvez aussi en perdre et Twitter vous sanctionne sévèrement. 'montrer moins ce contenu' ou blocage du compte = -77 points, compte signalé = -369 points. Attention aussi aux fautes d'orthographes qui peuvent faire excluse votre Tweet.",
+    sm_desc: "500 millions de tweets sont postés chaque jour sur Twitter. Mais alors comment faire en sorte que son post soit visible dans ce flot d'information ?",
+    intro: "500 millions de tweets sont postés chaque jour sur Twitter. Mais alors comment faire en sorte que son post soit visible dans ce flot d'information ? Il faut savoir que l'algorithme de Twitter note chaque post selon un barème. Selon les interactions qu'il reçoit, celui-ci obtient un score, et plus le score est grand, plus il a de chance d'être mise en avant. Un like = 0.5 point, un retweet = 1 point, 1 clic + 1 like + 1 commentaire = 11 points si vous répondez à un commentaire laissé sur votre post = 75 points. Attention, si vous pouvez gagner des points, vous pouvez aussi en perdre et Twitter vous sanctionne sévèrement. 'montrer moins ce contenu' ou blocage du compte = -77 points, compte signalé = -369 points. Attention aussi aux fautes d'orthographes qui peuvent faire exclure votre Tweet.",
     quote: "Le meilleur endroit pour cacher un corps, c’est la deuxième page de Google ! - Proverbe de référenceurs",
     tipsTitle: "Trois astuces pour augmenter la visibilité de vos posts Twitter :",
-    tips1: "Interagissez avec votre audience, mais pas que... : Lorsque votre post reçoit des commentaires, likez mais surtout RÉPONDEZ ! Une réponse vaut plus qu'un like. Votre compte et vos interactions sont aussi corrélées. Abonnez-vous aux pages des influenceurs (attention à respecter un ration abonnés / abonnements équilibré) et interagissez régulièrement avec eux pour améliorer votre visibilité. Bien qu'il soit plus difficile de les engager en conversation, vous êtes sûr d'obtenir une meilleure visibilité en vous connectant avec des profils influents et pertinents pour votre activité.",
+    tips1: "Interagissez avec votre audience, mais pas que... : Lorsque votre post reçoit des commentaires, likez mais surtout RÉPONDEZ ! Une réponse vaut plus qu'un like. Votre compte et vos interactions sont aussi corrélées. Abonnez-vous aux pages des influenceurs (attention à respecter un ratio abonnés / abonnements équilibré) et interagissez régulièrement avec eux pour améliorer votre visibilité. Bien qu'il soit plus difficile de les engager en conversation, vous êtes sûr d'obtenir une meilleure visibilité en vous connectant avec des profils influents et pertinants pour votre activité.",
     tips2:"Soyez régulier dans vos publications : Contrairement à Facebook, Twitter nécessite une stratégie de contenu agressive. Publiez régulièrement pour augmenter la visibilité de vos tweets, mais attention Twitter lutte contre les spams (NE JAMAIS mettre de lien dans vos Tweets), donc pensez à reformuler vos posts et limitez vous à 4 posts par jour maximum car au-delà Twitter ne les affichera pas. Organisez vos idées dans un calendrier éditorial et programmez vos tweets pour ne rien manquer. Publiez vos événements importants et partagez des tweets externes pour attirer une audience plus large.",
     tips3: "Ajouter des médias dans vos posts et des hashtags : Les tweets avec une vidéo sont partagés 6x plus que ceux avec des photos et 3x plus que ceux avec des GIFs et vous offre un boost de visibilité de x2. Les publications avec photo ou vidéo ont 50 % plus d'engagement. Les posts avec des médias ont 2x plus d'interactions que les textes sans médias. Les publications avec des médias ont plus de visibilité et d'impact auprès des abonnés. Tout comme les images, les tweets avec des hashtags reçoivent généralement plus d'engagement que ceux qui n'en ont pas. L'ajout de quelques hashtags peut rapidement augmenter la probabilité que de nouveaux abonnés trouvent votre compte. Utilisez des hashtags de votre secteur d'activité, des hashtags populaires de la communauté et des hashtags basés sur des événements pour donner de la personnalité à votre flux.",
     conclusionTitle: "Bonus : Twitter Blue Booste t'il les Tweet ?",
@@ -81,7 +77,6 @@ const blog_data = [
     home_4:true,
     large:true,
   },
-
 ]
 
 export default blog_data;
